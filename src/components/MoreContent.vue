059f9056d9c5a2137cc6f34a61e8ba13d85f9b37
937bb <3535025404@qq.com>
2025-06-18 19:05:53 +0800

    更新配置信息

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,15 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 937bb
+ * @Date: 2025-06-18 18:36:09
+ * @LastEditors: 937bb
+ * @LastEditTime: 2025-06-18 18:43:28
+-->
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">
+    让自己简单点
+  </div>
 </template>
 
 <style lang="scss" scoped>
